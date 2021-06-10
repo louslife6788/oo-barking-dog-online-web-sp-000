524edf8d3ab54a5fc3802a2285a5ff67962a808c
luis leon <leonluis380@yahoo.com>
2021-06-10 17:50:54 -0400

    commit tree 72fff20712c0f9a175257c202120b0d8d18072ea

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Dog
+
+def name=(label)
+    @this_dogs_name = label
+end
+
+def name
+    @this_dogs_name
+end
+
+
+
+
+
+
+def bark
+    puts"woof!"
+
+   
+end    
+
+end
